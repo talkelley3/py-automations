8038afb1a968a6f6e4179cb626db47168ac49e27
Talmage Kelley III <talkelley3@outlook.com>
2024-03-13 08:55:09 -0500

    Added CS script

---
+++ localAdminCSKiller/main.py
@@ -0,0 +1,15 @@
+from falconpy import RealTimeResponse
+
+def authToApi():
+    try: 
+        
+
+
+
+
+def main():
+    print("Welcome to the Crowdstrike Local Admin Removal Tool")
+    authToApi()
+
+if __name__ == "__main__":
+    main()
